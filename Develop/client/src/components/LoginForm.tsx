9fa5a5eeec39f01a11cd308912dbbfe1004765db
tlesner <tjlesner@gmail.com>
2025-04-22 20:54:38 -0600

    reformatted login form

--- Develop/client/src/components/LoginForm.tsx
+++ Develop/client/src/components/LoginForm.tsx
@@ -2,94 +2,105 @@
 import { useState } from 'react';
 import type { ChangeEvent, FormEvent } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
 
-import { loginUser } from '../utils/API';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
-import type { User } from '../models/User';
 
 // biome-ignore lint/correctness/noEmptyPattern: <explanation>
 const LoginForm = ({}: { handleModalClose: () => void }) => {
-  const [userFormData, setUserFormData] = useState<User>({ username: '', email: '', password: '', savedBooks: [] });
-  const [validated] = useState(false);
-  const [showAlert, setShowAlert] = useState(false);
+	const [formData, setFormData] = useState({
+		email: '',
+		password: '',
+	});
 
-  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
-  };
+	const [validated] = useState(false);
+	const [showAlert, setShowAlert] = useState(false);
 
-  const handleFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+	const [login, { error }] = useMutation(LOGIN_USER);
 
-    // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+	const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = event.target;
+		setFormData({ ...formData, [name]: value });
+	};
 
-    try {
-      const response = await loginUser(userFormData);
+	const handleFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+		// check if form has everything (as per react-bootstrap docs)
+		const form = event.currentTarget;
+		if (form.checkValidity() === false) {
+			event.preventDefault();
+			event.stopPropagation();
+			return;
+		}
 
-      const { token } = await response.json();
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
+		try {
+			const { data } = await login({ variables: { ...formData } });
 
-    setUserFormData({
-      username: '',
-      email: '',
-      password: '',
-      savedBooks: [],
-    });
-  };
+			if (error || !data || !data.login || !data.login.token) {
+				throw new Error('something went wrong!');
+			}
+			Auth.login(data?.login?.token);
+		} catch (err) {
+			console.error(err);
+			setShowAlert(true);
+		}
 
-  return (
-    <>
-      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-          Something went wrong with your login credentials!
-        </Alert>
-        <Form.Group className='mb-3'>
-          <Form.Label htmlFor='email'>Email</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Your email'
-            name='email'
-            onChange={handleInputChange}
-            value={userFormData.email || ''}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
-        </Form.Group>
+		setFormData({
+			email: '',
+			password: '',
+		});
+	};
 
-        <Form.Group className='mb-3'>
-          <Form.Label htmlFor='password'>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Your password'
-            name='password'
-            onChange={handleInputChange}
-            value={userFormData.password || ''}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
-        </Form.Group>
-        <Button
-          disabled={!(userFormData.email && userFormData.password)}
-          type='submit'
-          variant='success'>
-          Submit
-        </Button>
-      </Form>
-    </>
-  );
+	return (
+		<>
+			<Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+				<Alert
+					dismissible
+					onClose={() => setShowAlert(false)}
+					show={showAlert}
+					variant="danger">
+					Something went wrong with your login credentials!
+				</Alert>
+				<Form.Group className="mb-3">
+					<Form.Label htmlFor="email">Email</Form.Label>
+					<Form.Control
+						type="text"
+						placeholder="Your email"
+						name="email"
+						onChange={handleInputChange}
+						value={formData.email || ''}
+						required
+					/>
+					<Form.Control.Feedback type="invalid">
+						Email is required!
+					</Form.Control.Feedback>
+				</Form.Group>
+
+				<Form.Group className="mb-3">
+					<Form.Label htmlFor="password">Password</Form.Label>
+					<Form.Control
+						type="password"
+						placeholder="Your password"
+						name="password"
+						onChange={handleInputChange}
+						value={formData.password || ''}
+						required
+					/>
+					<Form.Control.Feedback type="invalid">
+						Password is required!
+					</Form.Control.Feedback>
+				</Form.Group>
+				<Button
+					disabled={!(formData.email && formData.password)}
+					type="submit"
+					variant="success">
+					Submit
+				</Button>
+			</Form>
+		</>
+	);
 };
 
 export default LoginForm;
