c7431f05045f9985a1ae043911b546746356a7ef
tlesner <tjlesner@gmail.com>
2025-04-22 22:29:06 -0600

    pulled files out of Develop

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Challenge18-BookSearch
+
+test
--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,7 @@ import type { Request, Response } from 'express';
 import db from './config/connection.js';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
-import { typeDefs, resolvers } from './schemas/index';
+import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './utils/auth.js';
 import { fileURLToPath } from 'node:url';
 
